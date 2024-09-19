aad3c289720aeae2ea7f10ec02b0b8dc45e5e748
erstazi <erstazi@c-wd.net>
2024-09-18 21:41:11 -0400

    Adding custom color by .setgreet color #ffffff or whatever color the player wants

--- init.lua
+++ init.lua
@@ -11,9 +11,22 @@
 
 local mod_name = minetest.get_current_modname()
 local mod_storage = minetest.get_mod_storage()
-mod_storage:set_string("greeting","1")
-mod_storage:set_string("next","0")
-mod_storage:set_string("debug","0")
+local greeting_setting = mod_storage:get_string("greeting")
+local next = mod_storage:get_string("next")
+local next = mod_storage:get_string("debug")
+local player_color = mod_storage:get_string("color")
+if greeting_setting == nil then
+  mod_storage:set_string("greeting","1")
+end
+if next == nil then
+  mod_storage:set_string("next","0")
+end
+if debug == nil then
+  mod_storage:set_string("debug","0")
+end
+if player_color == nil then
+  mod_storage:set_string("color","#FFFFFF")
+end
 
 local vc_version = "$Id: init.lua,v 1.0 2024-09-18 13:45:25 minetest Exp $"
 
@@ -29,7 +42,7 @@ greeting.default_lang = "en"
 greeting.modname = "GreetingCSM"
 greeting.delay = 20
 greeting.debug = true
-greeting.MY_COLOR = "#CCFFFF"
+-- greeting.MY_COLOR = "#CCFFFF"
 greeting.data = {
   ["en"] = "Welcome to Linux-Forks! Please, read the rules at https://li-fo.de/rules ! If you would like a free apartment with free food (take as you need) then type /phw ",
   ["de"] = "Willkommen bei Linux-Forks! Bitte lesen Sie die Regeln unter https://li-fo.de/rules-de ! Wenn Sie eine freie Wohnung mit kostenlosem Essen wünschen, dann verwenden Sie den Befehl: /phw ",
@@ -53,11 +66,12 @@ greeting.send_greeting = function(username, message, override)
   local now = minetest.get_us_time()
   local ref = mod_storage:get_string("next")
   local debug = mod_storage:get_string("debug")
-  local greetingSetting = mod_storage:get_string("greeting")
+  local greeting_setting = mod_storage:get_string("greeting")
+  local player_color = mod_storage:get_string("color")
   local diff = ref - now
   ref = tonumber(ref) or 0
   if now > ref or override == true then
-    minetest.send_chat_message(minetest.get_color_escape_sequence(greeting.MY_COLOR) .. username .. ": " .. message)
+    minetest.send_chat_message(minetest.get_color_escape_sequence(player_color) .. username .. ": " .. message)
     local wait_to_next = tostring(now + tonumber(greeting.delay) * 1000000)
     mod_storage:set_string("next", wait_to_next)
     if tonumber(debug) == 1 then
@@ -140,42 +154,73 @@ minetest.register_chatcommand('setgreet', {
   description = 'enable or disable greeting new players',
   func = function(param)
     local debug = mod_storage:get_string("debug")
-    if not param or param == "" or param == "status" then
-      local statustext = mod_storage:get_string("greeting")
-      if statustext == "0" then
-        statustext = "off"
-      else
-        statustext = "on"
-      end
-      return true,statustext
+    local player_color = mod_storage:get_string("color")
+    local paramsTable = {}
+    for paramItem in string.gmatch(param, "%S+") do
+      table.insert(paramsTable, paramItem)
+    end
+
+    if paramsTable[1] == nil or paramsTable[1] == "help" then
+      greeting.print("setgreet [0|1|on|off|enable|disable|reset|debug|color|status|help]")
     else
-      if param == "1" or param == "on" or param == "enable" then
-        mod_storage:set_string("greeting","1")
-        mod_storage:set_string("next","0")
-        return true,"Greeting new players is enabled"
-      end
-      if param == "0" or param == "off" or param == "disable" then
-        mod_storage:set_string("greeting","0")
-        mod_storage:set_string("next","0")
-        return true,"Greeting new players is disabled "
-      end
-      if param == "reset" then
-        mod_storage:set_string("next","0")
-        return true,"Greeting wait time has been reset "
+      if paramsTable[1] == "status" then
+        local greeting_setting = mod_storage:get_string("greeting")
+        if greeting_setting == "0" then
+          greeting_setting_text = "off"
+        else
+          greeting_setting_text = "on"
+        end
+        greeting.print(greeting_setting_text)
       end
 
-      if param == "debug" then
+      if paramsTable[1] == "1" or paramsTable[1] == "on" or paramsTable[1] == "enable" then
+        mod_storage:set_string("greeting","1")
+        mod_storage:set_string("next","0")
+        greeting.print("Greeting new players is enabled")
+      end
+
+      if paramsTable[1] == "0" or paramsTable[1] == "off" or paramsTable[1] == "disable" then
+        mod_storage:set_string("greeting","0")
+        mod_storage:set_string("next","0")
+        greeting.print("Greeting new players is disabled ")
+      end
+
+      if paramsTable[1] == "reset" then
+        mod_storage:set_string("next","0")
+        greeting.print("Greeting wait time has been reset ")
+      end
+
+      if paramsTable[1] == "debug" then
         greeting.print(tostring(debug))
         if tonumber(debug) == 1 then
           mod_storage:set_string("debug","0")
-          return true,"Greeting debug has been disabled "
+          greeting.print("Greeting debug has been disabled ")
         else
           mod_storage:set_string("debug","1")
-          return true,"Greeting debug has been enabled "
+          greeting.print("Greeting debug has been enabled ")
         end
       end
 
-      return true,"setgreet [0|1|on|off|enable|disable|reset|debug|status]"
+      if paramsTable[1] == "color" then
+        if paramsTable[2] == nil then
+          if player_color == nil or player_color == "#FFFFFF" then
+            greeting.print("To set your text color, run: .setgreet color #FFFFFF ")
+          else
+            greeting.print("Your color is " .. player_color .. ". To set your text color, run: .setgreet color " .. player_color)
+          end
+        else
+          local new_color = paramsTable[2]
+          if new_color:len() == 7 then
+            mod_storage:set_string("color", paramsTable[2])
+            greeting.print("Your color is " .. new_color .. ".")
+          elseif new_color:len() == 6 then
+            mod_storage:set_string("color", "#" .. paramsTable[2])
+            greeting.print("Your color is #" .. new_color .. ".")
+          else
+            greeting.print("Please, set your color with a hex color like #FFFFFF.")
+          end
+        end
+      end
     end
   end,
 })
@@ -270,7 +315,7 @@ minetest.register_chatcommand("intro-en", {
   func = safe(function(param)
     local username = param
     local message = "Welcome to Linux-Forks! Please, read the rules at https://li-fo.de/rules ! If you would like a free apartment with free food (take as you need) then type /phw "
-    minetest.send_chat_message(minetest.get_color_escape_sequence(greeting.MY_COLOR) .. username .. ": " .. message)
+    minetest.send_chat_message(minetest.get_color_escape_sequence(player_color) .. username .. ": " .. message)
   end),
 })
 ]]
